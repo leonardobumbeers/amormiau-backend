7e16e1169610fd58957a22e924522c9569ab2675
Leonardo B Souza <leonardo.bumbeers@backcountry.com>
2022-02-24 20:14:13 -0300

    Organized faker in a function and adjusted Update tests

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -1,9 +1,6 @@
 const fetch = require('cross-fetch');
-const { faker } = require('@faker-js/faker');
-const path = require('path')
-require("dotenv").config({
-    path: path.join(__dirname, "../.env")
-});
+const userData = require('../util/userData');
+require("dotenv/config");
 
 beforeAll(async () => {
 
@@ -13,8 +10,8 @@ beforeAll(async () => {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            email: "test@admin.com",
-            password: "123456"
+            email: process.env.ADMIN_EMAIL,
+            password: process.env.ADMIN_PASSWORD
         })
     })
     const responseToken = await responseLogin.json()
@@ -26,47 +23,31 @@ describe('Testing users CRUD', () => {
 
     test('should register a new user', async () => {
 
-        const randomName = faker.name.findName();
-        const randomEmail = faker.internet.email(randomName, null, '@test.com');
-        const randomPassword = faker.internet.password();
-        const randomCpf = faker.random.number({ min: 10000000000, max: 99999999999 });
-        const randomRg = faker.random.number({ min: 100000000, max: 999999999 });
-        const randomBirthDate = faker.date.past(80, '2020-01-01');
-        const randomPhone = faker.phone.phoneNumber('## #####-####');
-        const randomAddress = faker.address.streetAddress()
-        const randomAddressCity = faker.address.city();
-        const randomAddressState = faker.address.state();
-
-        console.log(`
-        ${randomName}
-        ${randomAddress}
-        ${randomAddressCity}
-        ${randomAddressState}
-        `);
-
+        const user = new userData();
+        
         const response = await fetch('http://localhost:3000/signup', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                name: randomName,
-                email: randomEmail,
+                name: user.randomName,
+                email: user.randomEmail,
                 password: '123456',
-                cpf: randomCpf,
-                rg: randomRg,
-                birthDate: randomBirthDate,
-                phone: randomPhone,
-                address: randomAddress,
-                city: randomAddressCity,
-                state: randomAddressState
+                cpf: user.randomCpf,
+                rg: user.randomRg,
+                birthDate: user.randomBirthDate,
+                phone: user.randomPhone,
+                address: user.randomAddress,
+                city: user.randomAddressCity,
+                state: user.randomAddressState
                 // role: ""
             })
         })
         const responseBody = await response.json()
         expect(response.status).toBe(200)
-        expect(responseBody.data.name).toBe(randomName)
-        expect(responseBody.data.email).toBe(randomEmail)
+        expect(responseBody.data.name).toBe(user.randomName)
+        expect(responseBody.data.email).toBe(user.randomEmail)
         expect(responseBody.message).toBe('You have signed up successfully')
 
         const userId = responseBody.data._id
@@ -88,6 +69,37 @@ describe('Testing users CRUD', () => {
         expect(responseBody.data).toBeTruthy()
     })
 
+    test('should update registered user by id', async () => {
+
+        const user = new userData();
+
+        const response = await fetch(`http://localhost:3000/admin/user/${global.userId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': global.token
+            },
+            body: JSON.stringify({
+                name: user.randomName,
+                email: user.randomEmail,
+                password: '123456',
+                cpf: user.randomCpf,
+                rg: user.randomRg,
+                birthDate: user.randomBirthDate,
+                phone: user.randomPhone,
+                address: user.randomAddress,
+                city: user.randomAddressCity,
+                state: user.randomAddressState
+                // role: ""
+            })
+        })
+        const responseBody = await response.json()
+        expect(response.status).toBe(200)
+        expect(responseBody.data).toBeTruthy()
+        expect(responseBody.message).toBe('User has been updated successfully!')
+
+    })
+
     test('should delete registered user by id', async () => {
         const response = await fetch(`http://localhost:3000/admin/user/${global.userId}`, {
             method: 'DELETE',
@@ -102,30 +114,7 @@ describe('Testing users CRUD', () => {
         expect(responseBody.message).toBe('User has been deleted')
     })
 
-    test('should not update user by id', async () => {
-        const response = await fetch(`http://localhost:3000/admin/user/${global.userId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'x-access-token': global.token
-            },
-            body: JSON.stringify({
-                name: '',
-                // email: '', // email is required
-                // password: '',
-                // cpf: '', // cpf is required
-                // rg: '', // rg is required
-                // birthDate: '', // birthDate is required
-                // phone: '', // phone is required
-                // address: '', // address is required
-                // city: '', // city is required
-                // state: '', // state is required
-                // role: ""
-            })
-        })
-        const responseBody = await response.json()
-        expect(response.status).toBe(500)
-    })
+    
 
     test('should return a list of all users', async () => {
         const response = await fetch('http://localhost:3000/admin/users', {
@@ -138,5 +127,6 @@ describe('Testing users CRUD', () => {
         const users = await response.json();
         expect(response.status).toBe(200);
         expect(users.data).toBeTruthy();
+        console.log(JSON.stringify(users.data, null, 2))
     });
 });
---
+++ server/util/userData.js
@@ -0,0 +1,16 @@
+const { faker } = require('@faker-js/faker');
+
+
+module.exports = function () {
+
+    this.randomName = faker.name.findName();
+    this.randomEmail = faker.internet.email(this.randomName, null, 'test.com');
+    this.randomCpf = faker.random.number({ min: 10000000000, max: 99999999999 });
+    this.randomRg = faker.random.number({ min: 100000000, max: 999999999 });
+    this.randomBirthDate = faker.date.past(80, '01-01-2020');
+    this.randomPhone = faker.phone.phoneNumber('## #####-####');
+    this.randomAddress = faker.address.streetAddress();
+    this.randomAddressCity = faker.address.city();
+    this.randomAddressState = faker.address.state()
+}
+
