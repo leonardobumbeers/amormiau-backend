2494f1349ae8526e75c095e059cd10c1c74f90d1
leonardobumbeers <leo.brsouza@gmail.com>
2022-03-10 18:53:59 -0300

    commented admin creation

--- server/util/createAdminUser.js
+++ server/util/createAdminUser.js
@@ -1,32 +1,32 @@
-const User = require('../models/userModel');
-require("dotenv/config");
-const mongoose = require('mongoose');
+// const User = require('../models/userModel');
+// require("dotenv/config");
+// const mongoose = require('mongoose');
 
 
 
-// make a connection 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, { useUnifiedTopology: true });
+// // make a connection 
+// mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true }, { useUnifiedTopology: true });
 
-// get reference to database
-var db = mongoose.connection;
+// // get reference to database
+// var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+// db.on('error', console.error.bind(console, 'connection error:'));
 
-db.once('open', function () {
+// db.once('open', function () {
 
-    console.log("Connection Successful!");
+//     console.log("Connection Successful!");
 
-    const user = new User({
-        name: 'Admin',
-        email: process.env.ADMIN_EMAIL,
-        password: process.env.ADMIN_PASSWORD_HASHED,
-        role: 'admin',
-        accessToken: process.env.ADMIN_ACCESS_TOKEN
-    });
-    user.save()
+//     const user = new User({
+//         name: 'Admin',
+//         email: process.env.ADMIN_EMAIL,
+//         password: process.env.ADMIN_PASSWORD_HASHED,
+//         role: 'admin',
+//         accessToken: process.env.ADMIN_ACCESS_TOKEN
+//     });
+//     user.save()
 
 
-})
-console.log("Admin user created!")
-mongoose.connection.close();
+// })
+// console.log("Admin user created!")
+// mongoose.connection.close();
 
