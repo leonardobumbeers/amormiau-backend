b65f48ff68fa90d5711217ae116c72cb70bbb159
Leonardo B Souza <leo.brsouza@gmail.com>
2022-03-16 21:31:27 -0300

    added a image example om tmp/uploads folder

--- server/util/multer.js
+++ server/util/multer.js
@@ -3,18 +3,18 @@ const path = require("path");
 const crypto = require("crypto");
 
 const storageTypes = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
-    },
-    filename: (req, file, cb) => {
-      crypto.randomBytes(16, (err, hash) => {
-        if (err) cb(err);
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, "..", "..", "tmp", "uploads"));
+  },
+  filename: (req, file, cb) => {
+    crypto.randomBytes(16, (err, hash) => {
+      if (err) cb(err);
 
-        file.key = `${hash.toString("hex")}-${file.originalname}`;
+      file.key = `${hash.toString("hex")}-${file.originalname}`;
 
-        cb(null, file.key);
-      });
-    }
+      cb(null, file.key);
+    });
+  }
 });
 
 module.exports = {
